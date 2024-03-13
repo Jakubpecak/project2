bb35acfcac47141bd99adb71d37dd231d1333ede
Jakubpecak <kanaknakola021@gmail.com>
2024-03-13 21:40:44 +0100

    add modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,22 @@ const routes: Routes = [
     loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
   },
   {
+    path: 'dashboard',
+    loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: 'properties',
+    loadChildren: () => import('./modules/properties/properties.module').then(m => m.PropertiesModule)
+  },
+  {
+    path: 'calculator',
+    loadChildren: () => import('./modules/calculator/calculator.module').then(m => m.CalculatorModule)
+  },
+  {
+    path: 'charts',
+    loadChildren: () => import('./modules/charts/charts.module').then(m => m.ChartsModule)
+  },
+  {
     path: 'not-found',
     component: PageNotFoundComponent
   },
